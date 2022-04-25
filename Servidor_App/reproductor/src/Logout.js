66a97257637ccfcf380629bdec8e03659116f7d1
Camilo Hernandez M <camilo.hernandez2020@alu.uct.cl>
2022-04-24 23:34:08 -0400

    Button CSS
    css en botones "Log"

--- Servidor_App/reproductor/src/Logout.js
+++ Servidor_App/reproductor/src/Logout.js
@@ -4,7 +4,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 export const LogoutButton = () => {
     const { logout } = useAuth0();
 
-    return <button onClick={() => logout({returnTo: window.location.origin})}>
+    return <button class = "log"onClick={() => logout({returnTo: window.location.origin})}>
         Logout
         </button>
 }
